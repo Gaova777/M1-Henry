9f4b017a03fa11bb031745c5942966664b0b0779
Juan Diego <gaova777@utp.edu.co>
2022-05-24 18:38:38 -0500

    2-0 committ

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,12 +2,22 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  var suma = 0;
+
+  for(var i = 0; i<num.length;i++){
+    suma += num[i]*2**(num.length-1-i)
+  }
+  return suma;
 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  var x = num
+  let binario = []
+  while(x!=0){
+    
+  }
 }
 
 
